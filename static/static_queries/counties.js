8b74513173993f1eedaabf29b9c1398bfcf741f4
Justin Berman <justinberman95@gmail.com>
2018-07-08 20:41:09 -0500

    added static expected responses from RESTful api

---
+++ static/static_queries/counties.js
@@ -0,0 +1,33 @@
+export const counties_response = {
+  "num_results": 1, 
+  "objects": [
+    {
+      "charities": [
+        {
+          "accountability_rating": 9.3, 
+          "cause": "Human Services", 
+          "city_id": 1, 
+          "county_id": 1, 
+          "fight_poverty_rating": 8.3, 
+          "financial_rating": 6.2, 
+          "id": 1, 
+          "mission_statement": "Feed the hungry", 
+          "name": "West Regional Food Bank", 
+          "state": "West State", 
+          "zip_code": 11111
+        }
+      ], 
+      "cities": [
+        {
+          "county_id": 1, 
+          "id": 1, 
+          "name": "Austin"
+        }
+      ], 
+      "id": 1, 
+      "name": "Travis"
+    }
+  ], 
+  "page": 1, 
+  "total_pages": 1
+}
