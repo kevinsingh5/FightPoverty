88e6776ad322919159eb6565d0884d2f11338a46
Kevin Singh <singhk7@yahoo.com>
2018-07-11 18:01:39 -0500

    testing automated file copying into ec2 - 01

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -5,18 +5,13 @@ set -f
 server=$DEPLOY_SERVER
 array=(${server//,/ })
 
-#echo "Deploying project on server $DEPLOY_SERVER"
-#ssh -i $SSH_PRIVATE_KEY ubuntu@$DEPLOY_SERVER "cd FightingPoverty/api && docker ps"
-
-#Iterate servers for deploy and pull last commit
+#Iterate servers for deployment
 for i in "${!array[@]}"; do    
-    #echo "Deploying project on server ${array[i]}"
-    #echo "$SSH_PRIVATE_KEY" > ~/key.pem
-    #ssh -i ~/key.pem ubuntu@${array[i]} "cd FightingPoverty/api && docker ps"
-#done
      echo "Deploy project on server ${array[i]}"
      echo "Current files:"
      ls -al
-     #ssh ubuntu@${array[i]} "cd /var/www && git pull origin master"
+     echo "Copying file"
+     scp -r .gitlab-deploy.sh ubuntu@${array[i]}:FightingPoverty/
+     echo "Connecting to EC2 host..."
      ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps"
 done
