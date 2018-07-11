ef52705c6dbdd9995ba977da87aa95f3cfd391b2
Kevin Singh <singhk7@yahoo.com>
2018-07-11 16:22:05 -0500

    testing ec2 deployment script

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -9,8 +9,8 @@ array=(${server//,/ })
 #ssh -i $SSH_PRIVATE_KEY ubuntu@$DEPLOY_SERVER "cd FightingPoverty/api && docker ps"
 
 #Iterate servers for deploy and pull last commit
-for i in "${!array[@]}"do    
-    echo "Deploying project on server $DEPLOY_SERVER"
+for i in "${!array[@]}"; do    
+    echo "Deploying project on server ${array[i]}"
     ssh -i $SSH_PRIVATE_KEY ubuntu@${array[i]} "cd FightingPoverty/api && docker ps"
 done
 #      echo "Deploy project on server ${array[i]}"    
