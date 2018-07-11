56f55a17607f8ef7db45971018b33a9f51eab43b
Kevin Singh <singhk7@yahoo.com>
2018-07-11 16:12:52 -0500

    trying ec2 deployment script

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -3,13 +3,16 @@
 #Get servers list
 set -f
 server=$DEPLOY_SERVER
-#array=(${string//,/ })
+array=(${server//,/ })
 
-echo "Deploying project on server $DEPLOY_SERVER"
-ssh -i $SSH_PRIVATE_KEY ubuntu@$DEPLOY_SERVER "cd FightingPoverty/api && docker ps"
+#echo "Deploying project on server $DEPLOY_SERVER"
+#ssh -i $SSH_PRIVATE_KEY ubuntu@$DEPLOY_SERVER "cd FightingPoverty/api && docker ps"
 
 #Iterate servers for deploy and pull last commit
-#for i in "${!array[@]}"do    
+for i in "${!array[@]}"do    
+    echo "Deploying project on server $DEPLOY_SERVER"
+    ssh -i $SSH_PRIVATE_KEY ubuntu@${array[i]} "cd FightingPoverty/api && docker ps"
+done
 #      echo "Deploy project on server ${array[i]}"    
 #      ssh ubuntu@${array[i]} "cd /var/www && git pull origin master"
 #done
