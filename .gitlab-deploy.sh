c0e7a49b7e30e97e4fe46e3af728876e7252aaf2
Kevin Singh <singhk7@yahoo.com>
2018-07-12 18:15:07 -0500

    cleaning deployment script

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -10,10 +10,11 @@ for i in "${!array[@]}"; do
      echo "Deploying project on server ${array[i]}"
      echo "Current files:"
      ls -al
-     echo "Copying files"
-     scp -r api/main.py ubuntu@${array[i]}:FightingPoverty/api/
+     echo "Copying files..."
+     # scp -r backend/main.py ubuntu@${array[i]}:FightingPoverty/api/
      scp -r frontend/ ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
      # ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps" #&& docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -p 80:80 --name fightpoverty -t fp ~&"
-     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/frontend/react-app/ && docker ps && docker stop reactapp && docker rm reactapp && docker build -t react . && docker run -p 3000:3000 -d --name reactapp -t react && docker ps && docker logs reactapp > dockerreact.log"
+     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/frontend/react-app/ && docker ps && docker stop reactapp && docker rm reactapp && echo Removed Docker container! && docker build -t react . && docker run -p 3000:3000 -d --name reactapp -t react && docker ps && docker logs reactapp > dockerreact.log"
+     echo "Successfully deployed React app on EC2 Docker container"
 done
