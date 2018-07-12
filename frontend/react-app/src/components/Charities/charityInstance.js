82529402976f8edff50b96959c9a31ca5369f5ef
Chris Amini <chris.amini0@gmail.com>
2018-07-11 22:49:20 -0500

    fixed charityNavigator links

--- frontend/react-app/src/components/Charities/charityInstance.js
+++ frontend/react-app/src/components/Charities/charityInstance.js
@@ -30,15 +30,15 @@ class CharityInstance extends Component {
                   </div>
               </section>
 
-              <ul>
+              <ul> 
                 <li>Cause: {this.props.location.state.cause}</li>
                 <li>Address: {this.props.location.state.address}</li>
                 <li>City: {this.props.location.state.city.name}</li>
                 <li>County: {this.props.location.state.county.name}</li>
                 <li>State: {this.props.location.state.city.state}</li>
                 <li>Zip Code:  {this.props.location.state.zip_code}</li>
-                <li><a href= 'https://www.charitynavigator.org/index.cfm?bay=content.view&cpid=5593#rating'>CharityNavigator's Accountability Score</a>: {this.props.location.state.charity_navigator_accountability_score} </li>
-                <li><a href= 'https://www.charitynavigator.org/index.cfm?bay=content.view&cpid=5593#rating'>CharityNavigator's Financial Score</a>: {this.props.location.state.charity_navigator_financial_score}</li>
+                <li><a href= 'https://www.charitynavigator.org/index.cfm?bay=content.view&cpid=1093'>CharityNavigator's Accountability Score</a>: {this.props.location.state.charity_navigator_accountability_score} </li>
+                <li><a href= 'https://www.charitynavigator.org/index.cfm?bay=content.view&cpid=35'>CharityNavigator's Financial Score</a>: {this.props.location.state.charity_navigator_financial_score}</li>
                 <li>FightPoverty Rating: {this.props.location.state.fight_poverty_score}</li>
               </ul>
 
