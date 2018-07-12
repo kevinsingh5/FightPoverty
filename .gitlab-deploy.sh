04dfc8165622fbc1f0f7a8868a80324dd48b3f35
Kevin Singh <singhk7@yahoo.com>
2018-07-11 21:23:30 -0500

    created script for docker react app

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -14,5 +14,6 @@ for i in "${!array[@]}"; do
      scp -r api/main.py ubuntu@${array[i]}:FightingPoverty/api/
      scp -r frontend/ ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
-     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps" #&& docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -p 80:80 --name fightpoverty -t fp ~&"
+     # ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps" #&& docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -p 80:80 --name fightpoverty -t fp ~&"
+     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/frontend/react-app/ && docker ps && docker stop reactapp && docker rm reactapp && docker build -t react . && docker run -p 49160:8080 -d --name reactapp -t react && docker ps && docker logs reactapp"
 done
