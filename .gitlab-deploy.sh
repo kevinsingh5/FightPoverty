31faf5de17dfbe5ed5c91740d412d33981dd7f31
Kevin Singh <singhk7@yahoo.com>
2018-07-11 21:26:33 -0500

    saving docker react logs to file

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -15,5 +15,5 @@ for i in "${!array[@]}"; do
      scp -r frontend/ ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
      # ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps" #&& docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -p 80:80 --name fightpoverty -t fp ~&"
-     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/frontend/react-app/ && docker ps && docker stop reactapp && docker rm reactapp && docker build -t react . && docker run -p 49160:8080 -d --name reactapp -t react && docker ps && docker logs reactapp"
+     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/frontend/react-app/ && docker ps && docker stop reactapp && docker rm reactapp && docker build -t react . && docker run -p 49160:8080 -d --name reactapp -t react && docker ps && docker logs reactapp > dockerreact.log"
 done
