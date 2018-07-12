4e6d8a1b37bc65f1c146f8edac81e53a9b143b45
Kevin Singh <singhk7@yahoo.com>
2018-07-11 21:12:24 -0500

    added Dockerfile for react app

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -10,8 +10,9 @@ for i in "${!array[@]}"; do
      echo "Deploying project on server ${array[i]}"
      echo "Current files:"
      ls -al
-     echo "Copying file"
-     scp -r api/app.py ubuntu@${array[i]}:FightingPoverty/api/
+     echo "Copying files"
+     scp -r api/main.py ubuntu@${array[i]}:FightingPoverty/api/
+     scp -r frontend/ ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
      ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps" #&& docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -p 80:80 --name fightpoverty -t fp ~&"
 done
