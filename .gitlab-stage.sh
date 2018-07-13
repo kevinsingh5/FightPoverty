5bb9ca92f9605d3da774635587abea765453bb35
Kevin Singh <singhk7@yahoo.com>
2018-07-12 22:39:31 -0500

    automating backend tests

--- .gitlab-stage.sh
+++ .gitlab-stage.sh
@@ -11,7 +11,7 @@ for i in "${!array[@]}"; do
      echo "Current files:"
      ls -al
      echo "Copying backend files..."
-     scp -r backend/ ubuntu@${array[i]}:FightingPoverty/
+     scp -v -r backend/ ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
      ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/api && docker ps && docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -d -p 80:80 --name fightpoverty -t fp && docker ps"
      echo "Successfully deployed Flask app on EC2 Docker container"
