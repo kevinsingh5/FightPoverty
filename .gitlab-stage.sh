e56b52453b003a6fc5a5a78d1e01385cd33bd9d0
Kevin Singh <singhk7@yahoo.com>
2018-07-12 23:16:04 -0500

    moved backend tests into docker

--- .gitlab-stage.sh
+++ .gitlab-stage.sh
@@ -11,8 +11,8 @@ for i in "${!array[@]}"; do
      echo "Current files:"
      ls -al
      echo "Copying backend files..."
-     scp -v -r backend/ ubuntu@${array[i]}:FightingPoverty/
+     scp -r backend/ ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
-     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/backend && make install && make run && make test && echo ALL TESTS PASSING && cd ~/FightingPoverty/api && docker ps && docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -d -p 80:80 --name fightpoverty -t fp && docker ps"
+     ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/backend && docker ps && docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -d -p 80:80 --name fightpoverty -t fp && docker ps"
      echo "Successfully deployed Flask app on EC2 Docker container"
 done
