3aabeb1e5ccbc7f937d3f4a038e8eb3991907469
Justin Berman <justinberman95@gmail.com>
2018-07-17 18:04:51 -0500

    Highlight active item in nav bar

--- frontend/react-app/src/components/NavBar/NavBar.js
+++ frontend/react-app/src/components/NavBar/NavBar.js
@@ -8,6 +8,12 @@ class NavBar extends Component {
   render(){
 
 
+		const currentPath = window.location.pathname
+		const getNavItem = (activeElement) => {
+			return currentPath.includes(activeElement) ? "nav-item active" : "nav-item"
+		}
+
+
   return(
   <header>
     	<nav className="navbar navbar-expand-md navbar-dark fixed-top navbar-custom">
@@ -17,19 +23,19 @@ class NavBar extends Component {
         	</button>
         	<div className="collapse navbar-collapse" id="navbarCollapse">
          		<ul className="navbar-nav ml-auto">
-            		<li className="nav-item active"> 
+            		<li className={getNavItem("cities")}> 
               			<a className="nav-link " href="../cities">Cities<span className="sr-only">(current)</span></a>
            		 	</li>
-           			<li className="nav-item active">
+           			<li className={getNavItem("counties")}>
              			<a className="nav-link" href="../counties">Counties</a>
            			</li>
-            		<li className="nav-item active">
+            		<li className={getNavItem("charities")}>
             			<a className="nav-link " href="../charities">Charities</a>
             		</li>
-                <li className="nav-item active">
+                <li className={getNavItem("search")}>
                   <a className="nav-link" href="../search">Search</a>
                 </li>
-            		<li className="nav-item active">
+            		<li className={getNavItem("about")}>
              			<a className="nav-link" href="../about">About</a>
             		</li>
                 
