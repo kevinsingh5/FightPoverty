04997368bbaadb23e53b84cf92cf3291bc92f28a
yt4369 <ytang2015@gmail.com>
2018-07-17 19:55:16 -0500

    added maps and share butons

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,36 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 from flask_cors import CORS
+from sqlalchemy import *
+from sqlalchemy.orm import *
 
 
 
-db_name = os.environ.get("DB", "testdb")
-username = os.environ.get("USER", "root")
-password = os.environ.get("PASSWORD", "downing")
-hostname = os.environ.get("HOST", "mysql01")
-port = os.environ.get("PORT", "3306")
+# db_name = os.environ.get("DB", "testdb")
+# username = os.environ.get("USER", "root")
+# password = os.environ.get("PASSWORD", "downing")
+# hostname = os.environ.get("HOST", "mysql01")
+# port = os.environ.get("PORT", "3306")
 
 
-# db_name = os.environ.get("RDS_DB_NAME", "testdb")
-# username = os.environ.get("RDS_USERNAME", "root")
-# password = os.environ.get("RDS_PASSWORD", "password")
-# hostname = os.environ.get("RDS_HOSTNAME", "testdbinstance.cydh8jzkegid.us-west-2.rds.amazonaws.com")
+db_name = os.environ.get("RDS_DB_NAME", "testdb")
+username = os.environ.get("RDS_USERNAME", "root")
+password = os.environ.get("RDS_PASSWORD", "password")
+hostname = os.environ.get("RDS_HOSTNAME", "testdbinstance.cydh8jzkegid.us-west-2.rds.amazonaws.com")
 
 
 db_connection = ''
-if db_name and username and password and hostname and port:
+# if db_name and username and password and hostname and port:
     # Prod database
-    db_connection = 'mysql+mysqldb://' + username + ':' + password + '@' + hostname + ':' + port + '/' + db_name
-elif db_name and username and password and hostname:
+#    db_connection = 'mysql+mysqldb://' + username + ':' + password + '@' + hostname + ':' + port + '/' + db_name
+if db_name and username and password and hostname:
     # Test database
     db_connection = 'mysql://' + username + ':' + password + '@' + hostname + '/' + db_name
 else :
     # Local database
-    db_connection = 'sqlite:////tmp/test.db'
+    db_connection = 'sqlite:///C:\\Windows\\Temp\\test.db'
 
 
 app = Flask(__name__)
@@ -40,16 +42,17 @@ db = SQLAlchemy(app)
 
 ### TABLES
 class Charity(db.Model):
+    __tablename__ = 'Charity'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), index=True, unique=True, nullable=False)
     mission_statement = db.Column(db.String(1250))
     cause = db.Column(db.String(50))
 
-    city_id = db.Column(db.Integer, db.ForeignKey('city.id'), nullable=False)
-    city = db.relationship('City', backref=db.backref('charities', lazy='dynamic'))
+    city_id = db.Column(db.Integer, db.ForeignKey('City.id'), nullable=False)
+    city = db.relationship('City', backref=db.backref('Charity', lazy='dynamic'))
 
-    county_id = db.Column(db.Integer, db.ForeignKey('county.id'), nullable=False)
-    county = db.relationship('County', backref=db.backref('charities', lazy='dynamic'))
+    county_id = db.Column(db.Integer, db.ForeignKey('County.id'), nullable=False)
+    county = db.relationship('County', backref=db.backref('Charity', lazy='dynamic'))
 
     zip_code = db.Column(db.Integer)
     address = db.Column(db.String(80))
@@ -65,13 +68,14 @@ class Charity(db.Model):
 
 
 class City(db.Model):
+    __tablename__ = 'City'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), index=True, nullable=False)
 
     state = db.Column(db.String(40))
 
-    county_id = db.Column(db.Integer, db.ForeignKey('county.id'), nullable=False)
-    county = db.relationship('County', backref=db.backref('cities', lazy='dynamic'))
+    county_id = db.Column(db.Integer, db.ForeignKey('County.id'), nullable=False)
+    county = db.relationship('County', backref=db.backref('City', lazy='dynamic'))
 
     __table_args__ = (
         # Must have unique city/state combo
@@ -80,6 +84,7 @@ class City(db.Model):
 
 
 class County(db.Model):
+    __tablename__ = 'County'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), index=True, nullable=False)
 
@@ -120,7 +125,16 @@ except Exception as exp:
     print('Failed to drop/create tables', exp)
 
 
+@app.route("/charity/city", methods=['GET'])
+def get_charity_by_city_name():
+    # take input_body and run SQL query to get charity by city name
+
+    # return result of SQL query
+    print(request.args.get('name', '') + " fdfdfdff")
+    return str(City.query.order_by(City.name).all())
+
+
 if __name__ == "__main__":
     app.debug = False
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0')
     
--- frontend/react-app/src/components/Counties/countyInstance.js
+++ frontend/react-app/src/components/Counties/countyInstance.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import CityCard from '../CityModel/CityCard.js';
 import CharityCard from '../CharityModel/CharityCard.js';
 import {getSpecificCharity} from '../../queries/charityQueries';
 import {getSpecificCity} from '../../queries/cityQueries';
+
+var mapBounds = [0, 0, 0, 0];
 
 
 class CountyInstance extends Component {
@@ -20,8 +23,21 @@ class CountyInstance extends Component {
     const charity = await getSpecificCharity(this.props.location.state.charities[0].name)
     const city = await getSpecificCity(this.props.location.state.cities[0].name);
 
+    console.log(this.props.location.state.name);
+    var this2 = this;
+    // search for city coordinates
+    $.getJSON('https://nominatim.openstreetmap.org/search?q=' + this.props.location.state.name + ',+' + this.props.location.state.state + '&format=json', function(data) {
+      // do stuff with the data
+      console.log(data);
+      if(data.length > 0) {
+        var loc = data[0];
+        //console.log(location);
+        mapBounds = loc['boundingbox'];
+        this2.setState({ charityInfo: charity, cityInfo: city});
+      }
+    });
 
-    this.setState({ charityInfo: charity, cityInfo: city});
+    //this.setState({ charityInfo: charity, cityInfo: city});
   }
 
   //scroll to top after clicking on card
@@ -43,12 +59,31 @@ class CountyInstance extends Component {
                   </div>
               </section>
 
+              <div class="row-fluid">
+                <div class="span4 text-right">
+                  <a class="twitter-share-button"
+                    href={"https://twitter.com/intent/tweet?text=" + this.props.location.state.name + "%20has%20a%20poverty%20rate%20of%20" + this.props.location.state.county_poverty_percentage + "%25%3a%0A"}
+                    data-size="large">
+                  Tweet</a>
+                  <br/>
+                  <div class="fb-share-button"
+                    data-href={document.URL}
+                    data-layout="button_count" data-size="large">
+                  </div>
+                </div>
+              </div>
+
               <ul>
                 <li>Cities: {this.props.location.state.cities[0].name} </li> 
                 <li>State: {this.props.location.state.state}</li>
                 <li>Poverty Population: {this.props.location.state.county_poverty_population} </li>
                 <li>Poverty Percentage: {this.props.location.state.county_poverty_percentage}%</li>
               </ul>
+              <div align="center">
+                <iframe width='600' height='450' src={"https://www.openstreetmap.org/export/embed.html?bbox=" + mapBounds[2] + "%2C" + mapBounds[0] + "%2C" + mapBounds[3] + "%2C" + mapBounds[1]} frameBorder="0" allowFullScreen></iframe>
+                <br/>
+                <small><a href={"https://www.openstreetmap.org/search?query=" + this.props.location.state.name + "%2C%20" + this.props.location.state.state + "#map=13"}>View Larger Map</a></small>
+              </div>
               <h1 align="center"> Cities related to {this.props.location.state.name}</h1>
              <div align="center">
                 {this.state.cityInfo.splice(0,1).map((dynamicCity, i) => <CityCard 
