f60f4ea95497ab77df8493d8af91418c2021c016
Kevin Singh <singhk7@yahoo.com>
2018-07-23 21:45:04 -0500

    temp disabling sync to server

--- .gitlab-stage.sh
+++ .gitlab-stage.sh
@@ -12,7 +12,7 @@ for i in "${!array[@]}"; do
      ls -al
      echo "Copying backend files..."
      # scp -r backend/ ubuntu@${array[i]}:FightingPoverty/
-     rsync -auv --delete backend ubuntu@${array[i]}:FightingPoverty/
+     # rsync -auv --delete backend ubuntu@${array[i]}:FightingPoverty/
      echo "Connecting to EC2 host..."
      ssh ubuntu@${array[i]} "pwd && cd FightingPoverty/backend && docker ps && docker system prune -f && docker stop fightpoverty && docker rm fightpoverty && docker build -t fp . && docker run --link mysql01:mysql -d -p 80:80 --name fightpoverty -t fp && docker ps"
      echo "Successfully deployed Flask app on EC2 Docker container"
